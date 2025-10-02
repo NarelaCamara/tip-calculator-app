f34f50f57317cfc3138116ffafb0a6bcc335c3b0
RoxanaCamara <roxana.camara07@gmail.com>
2025-10-02 15:51:18 -0300

    feat:  style letters

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   const percent = [5, 10, 15, 25, 50];
 
   return (
-    <div className="bg-[#9FE8DF]">
+    <div className="bg-[#9FE8DF] flex flex-col items-center justify-center min-h-screen ">
       <img src={Logo} className="p-8" alt="Vite logo" />
-      <div className="py-8 px-6 bg-white">
+      <div className="py-8 px-6 bg-white rounded-xl">
         <h2 className="text-[#5E7A7D] text-xl ">Bill</h2>
         <input
           type="number"
@@ -16,23 +16,25 @@ function App() {
           className="bg-[#9EBBBD] rounded-lg p-2"
         />
 
-        <h2 className="text-[#5E7A7D] text-xl ">Select Tip %</h2>
-        <div>
-          {percent.map((e) => (
-            <button
-              type="button"
-              className="bg-[#00474B] py-2 px-7 rounded-sm m-2"
-              onClick={() => console.log("e", e)}
-            >
-              <p className="text-white text-xl ">{e}%</p>
-            </button>
-          ))}
-          <input
-            type="number"
-            name="bill"
-            id="bill"
-            className="bg-[#9EBBBD] rounded-lg p-2"
-          />
+        <div className="my-8">
+          <h2 className="text-[#5E7A7D] text-xl ">Select Tip %</h2>
+          <div>
+            {percent.map((e) => (
+              <button
+                type="button"
+                className="bg-[#00474B] py-2 px-7 rounded-sm m-2"
+                onClick={() => console.log("e", e)}
+              >
+                <p className="text-white text-xl ">{e}%</p>
+              </button>
+            ))}
+            <input
+              type="number"
+              name="bill"
+              id="bill"
+              className="bg-[#9EBBBD] rounded-lg p-2"
+            />
+          </div>
         </div>
 
         <h2 className="text-[#5E7A7D] text-xl ">Number of People</h2>
@@ -43,7 +45,7 @@ function App() {
           className="bg-[#9EBBBD] rounded-lg p-2"
         />
 
-        <div className=" bg-[#00474B] p-6 rounded-xl">
+        <div className=" bg-[#00474B] p-6 rounded-xl mt-8">
           <div>
             <p className="text-white text-xl ">Tip Amount</p>
             <p className="text-white text-xl ">/ person</p>
