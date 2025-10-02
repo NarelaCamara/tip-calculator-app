3fa00756a8f62b02689640ff83b08b327b3a830f
RoxanaCamara <roxana.camara07@gmail.com>
2025-10-02 16:10:59 -0300

    feat: styles

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,25 @@ function App() {
     <div className="bg-[#9FE8DF] flex flex-col items-center justify-center min-h-screen ">
       <img src={Logo} className="p-8" alt="Vite logo" />
       <div className="py-8 px-6 bg-white rounded-xl">
-        <h2 className="text-[#5E7A7D] text-xl ">Bill</h2>
+        <h2 className="text-[#5E7A7D] text-[16px] ">Bill</h2>
         <input
           type="number"
           name="bill"
           id="bill"
-          className="bg-[#9EBBBD] rounded-lg p-2"
+          placeholder="0"
+          min={0}
+          inputMode="numeric"
+          className="bg-[#9EBBBD] rounded-lg p-2 text-2xl text-[#00474B]  [&::-webkit-inner-spin-button]:appearance-none
+                [&::-webkit-outer-spin-button]:appearance-none
+                [appearance:textfield]"
         />
 
         <div className="my-8">
-          <h2 className="text-[#5E7A7D] text-xl ">Select Tip %</h2>
+          <h2 className="text-[#5E7A7D] text-[16px] ">Select Tip %</h2>
           <div>
             {percent.map((e) => (
               <button
+                key={e}
                 type="button"
                 className="bg-[#00474B] py-2 px-7 rounded-sm m-2"
                 onClick={() => console.log("e", e)}
@@ -29,40 +35,49 @@ function App() {
               </button>
             ))}
             <input
-              type="number"
+              inputMode="numeric"
               name="bill"
+              min={0}
+              placeholder="custom"
               id="bill"
-              className="bg-[#9EBBBD] rounded-lg p-2"
+              className="bg-[#9EBBBD] rounded-lg p-2 text-2xl  text-[#00474B] [&::-webkit-inner-spin-button]:appearance-none
+                [&::-webkit-outer-spin-button]:appearance-none
+                [appearance:textfield]"
             />
           </div>
         </div>
 
-        <h2 className="text-[#5E7A7D] text-xl ">Number of People</h2>
+        <h2 className="text-[#5E7A7D] text-[16px] ">Number of People</h2>
         <input
           type="number"
           name="bill"
           id="bill"
-          className="bg-[#9EBBBD] rounded-lg p-2"
+          placeholder="0"
+          min={0}
+          inputMode="numeric"
+          className="bg-[#9EBBBD] rounded-lg p-2 text-2xl text-[#00474B]   [&::-webkit-inner-spin-button]:appearance-none
+                [&::-webkit-outer-spin-button]:appearance-none
+                [appearance:textfield]"
         />
 
         <div className=" bg-[#00474B] p-6 rounded-xl mt-8">
           <div>
-            <p className="text-white text-xl ">Tip Amount</p>
-            <p className="text-white text-xl ">/ person</p>
-            <p className="text-white text-xl ">$0.00</p>
+            <p className="text-white text-[16px] ">Tip Amount</p>
+            <p className="text-[#7F9D9F] text-[13px] ">/ person</p>
+            <p className="text-white text-4xl ">$0.00</p>
           </div>
 
           <div>
-            <p className="text-white text-xl ">Total</p>
-            <p className="text-white text-xl ">/ person</p>
-            <p className="text-white text-xl ">$0.00</p>
+            <p className="text-white  text-[16px] ">Total</p>
+            <p className="text-[#7F9D9F] text-[13px] ">/ person</p>
+            <p className="text-white text-4xl ">$0.00</p>
           </div>
 
           <button
             type="button"
             className="bg-[#0D686D] py-2 px-7 rounded-sm m-2"
           >
-            <p className="text-white text-xl ">Reset</p>
+            <p className="text-white text-xl ">RESET</p>
           </button>
         </div>
       </div>
