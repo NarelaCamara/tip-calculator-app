f34b5fb37c7b062d8a2497e16a75bac340cc1338
RoxanaCamara <roxana.camara07@gmail.com>
2025-10-02 14:34:38 -0300

    feaT: maquetado

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,67 @@ import Logo from "./assets/logo.svg";
 import "./App.css";
 
 function App() {
+  const percent = [5, 10, 15, 25, 50];
+
   return (
     <div className="bg-[#9FE8DF]">
-      <div className="p-4">
-        <img src={Logo} className="logo" alt="Vite logo" />
+      <img src={Logo} className="p-8" alt="Vite logo" />
+      <div className="py-8 px-6 bg-white">
+        <h2 className="text-[#5E7A7D] text-xl ">Bill</h2>
+        <input
+          type="number"
+          name="bill"
+          id="bill"
+          className="bg-[#9EBBBD] rounded-lg p-2"
+        />
+
+        <h2 className="text-[#5E7A7D] text-xl ">Select Tip %</h2>
+        <div>
+          {percent.map((e) => (
+            <button
+              type="button"
+              className="bg-[#00474B] py-2 px-7 rounded-sm m-2"
+              onClick={() => console.log("e", e)}
+            >
+              <p className="text-white text-xl ">{e}%</p>
+            </button>
+          ))}
+          <input
+            type="number"
+            name="bill"
+            id="bill"
+            className="bg-[#9EBBBD] rounded-lg p-2"
+          />
+        </div>
+
+        <h2 className="text-[#5E7A7D] text-xl ">Number of People</h2>
+        <input
+          type="number"
+          name="bill"
+          id="bill"
+          className="bg-[#9EBBBD] rounded-lg p-2"
+        />
+
+        <div className=" bg-[#00474B] p-6 rounded-xl">
+          <div>
+            <p className="text-white text-xl ">Tip Amount</p>
+            <p className="text-white text-xl ">/ person</p>
+            <p className="text-white text-xl ">$0.00</p>
+          </div>
+
+          <div>
+            <p className="text-white text-xl ">Total</p>
+            <p className="text-white text-xl ">/ person</p>
+            <p className="text-white text-xl ">$0.00</p>
+          </div>
+
+          <button
+            type="button"
+            className="bg-[#0D686D] py-2 px-7 rounded-sm m-2"
+          >
+            <p className="text-white text-xl ">Reset</p>
+          </button>
+        </div>
       </div>
     </div>
   );
